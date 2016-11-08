384df8e5600580c5139032a54574c5047fbb9973
cmm524 <cmm524@scarletmail.rutgers.edu>
2016-11-08 16:36:57 -0500

    Knight Movements complete

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -1,5 +1,7 @@
 package model;
 
+import chess.Chess;
+
 public class Knight extends Piece {
 	
 	public Knight(char color, String role, String location) {
@@ -7,7 +9,23 @@ public class Knight extends Piece {
 	}
 
 	@Override
-	public boolean isMoveValid(String origin, String destination, ChessBoard board) {
+	public boolean isMoveValid(String origin, String destination, ChessBoard Board) {
+		
+		int[] coordOg = Chess.stringToCoordinants(origin);
+		int[] coordDt = Chess.stringToCoordinants(destination);
+		
+		// make sure it is a valid L shape
+		if((Math.abs(coordOg[0] - coordDt[0]) == 2 && Math.abs(coordOg[1] - coordDt[1]) == 1)
+				|| (Math.abs(coordOg[0] - coordDt[0]) == 1 && Math.abs(coordOg[1] - coordDt[1]) == 2)){
+			
+			// Now have to check if there is a piece of the same color
+			if(Board.findPiece(destination) == null){
+				return true;
+			}else if(Board.findPiece(destination) != null && Board.findPiece(destination).getColor() != this.getColor()){
+				return true;
+			}
+		}
+		
 		return false;
 	}
 }
