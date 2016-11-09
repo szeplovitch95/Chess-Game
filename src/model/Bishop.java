5fa7140c33bbdc9a28c29525bffc9f202f3882ad
cmm524 <cmm524@scarletmail.rutgers.edu>
2016-11-08 22:06:02 -0500

    Working on Rook and Bishop moves. Shachar did most of the work here

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -1,13 +1,25 @@
 package model;
+import chess.Chess;
 
-public class Bishop extends Piece {
+public class Bishop extends Piece implements moveHelper{
 
 	public Bishop(char color, String role, String location) {
 		super(color, role, location);
 	}
 
 	@Override
-	public boolean isMoveValid(String origin, String destination, ChessBoard board) {
-		return false;
+	public boolean isMoveValid(String origin, String destination, ChessBoard Board) {
+		int[] originMoves = Chess.stringToCoordinants(origin);
+		int[] destinationMoves = Chess.stringToCoordinants(destination);
+		
+		if(Board.board[destinationMoves[0]][destinationMoves[1]].getPiece() != null) {
+			if(Board.board[originMoves[0]][originMoves[1]].getPiece().getColor() ==
+					Board.board[destinationMoves[0]][destinationMoves[1]].getPiece().getColor())
+			{
+				return false;
+			}			
+		}
+
+		return hasPiecesInbetween(origin, destination, Board.board);
 	}
 }
--- src/model/Rook.java
+++ src/model/Rook.java
@@ -1,6 +1,7 @@
 package model;
+import chess.Chess;
 
-public class Rook extends Piece {
+public class Rook extends Piece implements moveHelper{
 	private boolean isMoved;
 	
 	public Rook(char color, String role, String location) {
@@ -16,7 +17,19 @@ public class Rook extends Piece {
 	}
 
 	@Override
-	public boolean isMoveValid(String origin, String destination, ChessBoard board) {
-		return false;
+	public boolean isMoveValid(String origin, String destination, ChessBoard Board) {
+		int[] originMoves = Chess.stringToCoordinants(origin);
+		int[] destinationMoves = Chess.stringToCoordinants(destination);
+		
+		
+		if(Board.board[originMoves[0]][originMoves[1]].getPiece().getColor() ==
+						Board.board[destinationMoves[0]][destinationMoves[1]].getPiece().getColor())
+		{
+			return false;
+		}
+		
+		
+		
+		return hasPiecesInbetween(origin, destination, Board.board);
 	}
 }
